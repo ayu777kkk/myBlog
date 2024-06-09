1223d17da1d2d5f0bfa658697117c48a3a342750
lxy <3056053326@qq.com>
2024-06-09 05:06:08 -0400

    adding web part

---
+++ myblog-web/src/main/java/com/ayulin/myblog/web/MyblogWebApplication.java
@@ -0,0 +1,11 @@
+package com.ayulin.myblog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogWebApplication.class, args);
+    }
+}
